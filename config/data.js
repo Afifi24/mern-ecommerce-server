7e87c2a91f6aaeb93d453a0ab2319bf7dd250cc2
afifi brahim <afifi71brahim@gmail.com>
2024-02-05 23:16:28 +0100

    add database data

---
+++ config/data.js
@@ -0,0 +1,118 @@
+import {v4 as uuidv4} from 'uuid'
+import image1 from './assets/image1.jpg'
+import image2 from './assets/image2.jpg'
+import image3 from './assets/image3.jpg'
+import image4 from './assets/image4.jpg'
+import image5 from './assets/image5.jpg'
+import image6 from './assets/image6.jpg'
+import image7 from './assets/image7.jpg'
+import image8 from './assets/image8.jpg'
+import image9 from './assets/image9.jpg'
+import image10 from './assets/image10.jpg'
+import tshirt1 from './assets/tshirt1.jpg'
+import tshirt2 from './assets/tshirt2.jpg'
+import tshirt3 from './assets/tshirt3.jpg'
+import tshirt4 from './assets/tshirt4.jpg'
+import tshirt5 from './assets/tshirt5.jpg'
+
+
+
+
+export const men = [
+    {    
+        id:uuidv4(),
+        image:tshirt1,
+        title:'Tech-Ready Travel Tshirt',
+        price:65,
+        oldprice:100,
+        desc:'Gear up for your next adventure with our Tech-Ready Travel Tshirt. Packed with smart features like RFID protection, multiple pockets, and a convertible design, this tshirt is designed for the tech-savvy traveler.',
+        quantity:1,
+        link:'tshirt1.jpg'
+    },
+    {
+        id:uuidv4(),
+        image:image2,
+        title:'Heritage Denim Jacket',
+        price:66,
+        oldprice:118,
+        quantity:1,
+        desc:'Timeless style meets modern comfort in our Heritage Denim Jacket. Crafted from premium denim, this jacket combines a classic look with contemporary features, making it a staple for your casual wardrobe.',
+        link:'image2.jpg'
+
+
+    },
+    {
+        id:uuidv4(),
+        image:tshirt2,
+        title:'Tech-Ready Travel Tshirt',
+        price:50,
+        oldprice:90,
+        quantity:1,
+        desc:'Gear up for your next adventure with our Tech-Ready Travel Tshirt. Packed with smart features like RFID protection, multiple pockets, and a convertible design, this tshirt is designed for the tech-savvy traveler.',
+        link:'tshirt2.jpg'
+
+
+    },
+    {   
+        id:uuidv4(),
+        image:tshirt4,
+        title:'Tech-Ready Travel Tshirt',
+        price:82,
+        oldprice:120,
+        quantity:1,
+        desc:'Gear up for your next adventure with our Tech-Ready Travel Tshirt. Packed with smart features like RFID protection, multiple pockets, and a convertible design, this tshirt is designed for the tech-savvy traveler.',
+        link:'tshirt4.jpg'
+
+
+    },
+    {   
+        id:uuidv4(),
+        image:image6,
+        title:'Eco-Friendly Expedition Parka',
+        price:72,
+        oldprice:110,
+        quantity:1,
+        desc:'Timeless style meets modern comfort in our Heritage Denim Jacket. Crafted from premium denim, this jacket combines a classic look with contemporary features, making it a staple for your casual wardrobe.',
+        link:'image6.jpg'
+
+    },
+    
+    {
+        id:uuidv4(),
+        image:image3,
+        title:'Men Green solid Zippered Full-Zip Slim Fit Bomber Jacket',
+        price:88,
+        oldprice:120,
+        quantity:1,
+        desc:'A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.',
+        link:'image3.jpg'
+
+
+    },
+    {
+        id:uuidv4(),
+        image:image4,
+        title:'Heritage Denim Jacket',
+        price:95,
+        oldprice:140,
+        quantity:1,
+        desc:'A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.',
+        link:'image4.jpg'
+
+
+
+    },
+    {
+        id:uuidv4(),
+        image:image9,
+        title:'Men Green solid Zippered Full-Zip Slim Fit Bomber Jacket',
+        price:120,
+        oldprice:200,
+        quantity:1,
+        desc:'A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.',
+        link:'image9.jpg'
+
+
+    },
+  
+]
