c1dcf3350390a9ad67723ffb695e21a3279ed034
afifi brahim <afifi71brahim@gmail.com>
2024-02-09 23:47:03 +0100

    add insertWomenData

--- controllers/insertData.js
+++ controllers/insertData.js
@@ -1,6 +1,8 @@
 const Data = require('../models/dataSchema')
 const {men,women,kids,newCollections} = require('../config/data')
 
+
+// men
 const insertData = async(req,res)=>{
     try {
         Data.insertMany(men)
@@ -17,4 +19,21 @@ const getDatat = async(req,res)=>{
         console.log(error)
     }
 }
-module.exports = {insertData,getDatat}
+// women
+const insertWomenData = async(req,res)=>{
+    try {
+        Data.insertMany(women)
+        res.json('data posted')
+    } catch (error) {
+        console.log(error)
+    }
+}
+const getWomenData = async(req,res)=>{
+    try {
+        const data = await Data.find()
+        res.json(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = {insertData,getDatat,insertWomenData,getWomenData} 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const  express = require('express')
 const { LoginUser, RegisterUser } = require('../controllers/userController') 
-const {insertData,getDatat} = require('../controllers/insertData')
+const {insertData,getDatat,insertWomenData,getWomenData} = require('../controllers/insertData')
 
 const router = express.Router()
 
@@ -8,7 +8,8 @@ const router = express.Router()
 
 router.post('/register',RegisterUser)
 router.post('/login',LoginUser)
+router.post('/insertWomenData',insertWomenData)
+router.get('/getWomenData',getWomenData)
 router.post('/insertData',insertData)
 router.get('/getData',getDatat)
-
 module.exports = router
